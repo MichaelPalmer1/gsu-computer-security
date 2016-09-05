648cbd67cbc223d95804f5f8ad4bb34c21dc5690
Michael Palmer <michael@michaeldpalmer.com>
2016-09-05 12:49:07 -0400

    Initial commit

---
+++ PacketSniff/PacketSniff/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace PacketSniff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.cmbDevices = new System.Windows.Forms.ComboBox();
+            this.txtCapturedData = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartStop.Location = new System.Drawing.Point(170, 25);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(88, 39);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbDevices
+            // 
+            this.cmbDevices.FormattingEnabled = true;
+            this.cmbDevices.Location = new System.Drawing.Point(12, 97);
+            this.cmbDevices.Name = "cmbDevices";
+            this.cmbDevices.Size = new System.Drawing.Size(413, 21);
+            this.cmbDevices.TabIndex = 1;
+            // 
+            // txtCapturedData
+            // 
+            this.txtCapturedData.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCapturedData.Location = new System.Drawing.Point(12, 166);
+            this.txtCapturedData.Multiline = true;
+            this.txtCapturedData.Name = "txtCapturedData";
+            this.txtCapturedData.Size = new System.Drawing.Size(413, 303);
+            this.txtCapturedData.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 481);
+            this.Controls.Add(this.txtCapturedData);
+            this.Controls.Add(this.cmbDevices);
+            this.Controls.Add(this.btnStartStop);
+            this.Name = "Form1";
+            this.Text = "PacketSniff";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.ComboBox cmbDevices;
+        private System.Windows.Forms.TextBox txtCapturedData;
+    }
+}
+
